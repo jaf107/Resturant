3b7b3f3f31b1fb6d126f862b9235b3237eb9f5f8
Jafar Mahin <jafarmahin107@gmail.com>
2021-10-22 23:58:42 +0600

    Added comment forms

--- src/components/body/Menu.js
+++ src/components/body/Menu.js
@@ -1,14 +1,20 @@
 import React, {Component} from 'react';
-import DISHES from '../../data/dishes';
-import COMMENTS from '../../data/comments';
 import MenuItem from './MenuItem';
 import DishDetail from './DishDetail';
 import {CardColumns, Modal, ModalBody, ModalFooter, Button} from 'reactstrap';
+import {connect} from 'react-redux';
+
+const mapStateToProps = state =>{
+    return{
+        dishes: state.dishes,
+        comments: state.comments
+    }
+}
 
 class Menu extends Component{
     state = {
-        dishes: DISHES,
-        comments: COMMENTS,
+        // dishes: DISHES,
+        // comments: COMMENTS,
         selectedDish: null,
         modalOpen: false
     }
@@ -27,7 +33,7 @@ class Menu extends Component{
 
     render(){
         document.title = "Menu";
-        const menu = this.state.dishes.map(item => {
+        const menu = this.props.dishes.map(item => {
             return(
                 <MenuItem 
                     dish = {item} 
@@ -38,7 +44,7 @@ class Menu extends Component{
 
         let dishDetail = null;
         if(this.state.selectedDish != null){
-            const comments = this.state.comments.filter(comment =>{
+            const comments = this.props.comments.filter(comment =>{
                 return comment.dishId === this.state.selectedDish.id; 
             })
             dishDetail = <DishDetail 
@@ -53,7 +59,7 @@ class Menu extends Component{
                     <CardColumns>
                         {menu}
                     </CardColumns>
-                    <Modal isOpen = {this.state.modalOpen} onClick = {this.toggleModal} >
+                    <Modal isOpen = {this.state.modalOpen} >
                         <ModalBody>
                             {dishDetail}
                         </ModalBody>
@@ -69,4 +75,4 @@ class Menu extends Component{
     }
 }
 
-export default Menu;
+export default connect(mapStateToProps)(Menu);
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,3 +1,19 @@
-const reducer = (state, action ) =>{
+import DISHES from '../data/dishes';
+import COMMENTS from '../data/comments';
+
+const initialState = {
+    dishes: DISHES,
+    comments: COMMENTS,
+    sample: "Hello World"
+}
+
+export const Reducer = (state = initialState, action ) =>{
+    // console.log("From Reducer", action);
+    if(action.type === 'TEST'){
+        return{
+            ...state,
+            sample: action.str
+        }
+    }
     return state;
 }
